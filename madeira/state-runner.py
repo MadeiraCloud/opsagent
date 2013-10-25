652989d096c5b81a6b16c58c82d70ebcd3718624
Wang Xu <xu@mc2.io>
2013-10-25 18:47:26 +0800

    Try to run salt state with object instead of sls
      - with proper options setup
      - invoke salt.state.State directly
    
    Signed-off-by: Wang Xu <xu@mc2.io>

---
+++ madeira/state-runner.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+# using gevent for event loop and greenlet scheduling
+import gevent
+from gevent.threadpool import ThreadPool
+
+# using salt for provisioning
+from salt.state  import HighState, State
+from salt.minion import SMinion
+
+#for test only
+import json
+
+class StateRunException(Exception):
+    pass
+
+class StateRunner(object):
+
+    DEFAULT_POOL_SIZE = 2
+
+    def __init__(self, states = [], pool_size = DEFAULT_POOL_SIZE):
+        self._pool = ThreadPool(pool_size)
+
+        if not type(states) == list:
+            raise StateRunException('No state list to run')
+
+        self._states = states
+
+        self.highstate = None
+        self.state =None
+        self._init_state()
+        #print self.highstate.avail
+
+    def run(self):
+        for state in self._states:
+            ret = self.state.call_high(state)
+            print json.dumps(ret, sort_keys=True,
+                  indent=4, separators=(',', ': '))
+
+    def _init_state(self):
+        
+        def init_state():
+            salt_opts = {
+                'file_client':       'local',
+                'renderer':          'yaml_jinja',
+                'failhard':          False,
+                'state_top':         'salt://top.sls',
+                'nodegroups':        {},
+                'file_roots':        {'base': ['/srv/salt']},
+                'state_auto_order':  False,
+                'extension_modules': '/var/cache/salt/minion/extmods',
+                'id':                '',
+                'pillar_roots':      '',
+                'cachedir':          '/code/OpsAgent/cache',
+                'test':              False,
+                }
+            
+            self.state = State(salt_opts)
+
+            print json.dumps(salt_opts, sort_keys=True,
+                  indent=4, separators=(',', ': '))
+
+        self._pool.spawn(init_state)
+        try:
+            gevent.run()
+        except AttributeError:
+            gevent.wait()
+
+# codes for test
+def main():
+    predefine_states = {
+        'mypackages' : {
+            '__env__': 'base',
+            '__sls__': 'madeira',
+            'pkg' : [
+                {
+                    'pkgs': [
+                        'cowsay',
+                    ],
+                },
+                'installed',
+                {
+                    'order': 10000
+                }
+            ]
+        },
+    }
+
+    runner = StateRunner([predefine_states])
+    runner.run()
+
+if __name__ == '__main__':
+    main()
