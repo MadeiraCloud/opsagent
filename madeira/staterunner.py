f6447fcc68a7ba81765bf04e1be38a0d7c19e5c0
Wang Xu <xu@mc2.io>
2013-10-26 10:44:10 +0800

    StateRunner init cleanup
    - use lambda and asyncResult for _init_state
    - add set_states method
    
    Signed-off-by: Wang Xu <xu@mc2.io>

--- madeira/staterunner.py
+++ madeira/staterunner.py
@@ -5,11 +5,7 @@ import gevent
 from gevent.threadpool import ThreadPool
 
 # using salt for provisioning
-from salt.state  import HighState, State
-from salt.minion import SMinion
-
-#for test only
-import json
+from salt.state  import State
 
 class StateRunException(Exception):
     pass
@@ -18,55 +14,62 @@ class StateRunner(object):
 
     DEFAULT_POOL_SIZE = 2
 
-    def __init__(self, states = [], pool_size = DEFAULT_POOL_SIZE):
+    def __init__(self, opts, states = [], pool_size = DEFAULT_POOL_SIZE):
         self._pool = ThreadPool(pool_size)
 
         if not type(states) == list:
             raise StateRunException('No state list to run')
 
+        self._opts   = opts
         self._states = states
 
-        self.highstate = None
         self.state =None
         self._init_state()
-        #print self.highstate.avail
+
+    def get_opts(self):
+        return self._opts
+
+    def set_states(self, states = []):
+        if not type(states) == list:
+            raise StateRunException('No state list to run')
+        self._states = states
 
     def run(self):
+        state_results = []
         for state in self._states:
             ret = self._pool.spawn(self.state.call_high, state)
-            gevent.wait()
+            ret.wait()
+            state_results.append(ret.get())
+        return state_results
 
-            print json.dumps(ret.get(), sort_keys=True,
-                  indent=4, separators=(',', ': '))
+    def stop(self, timeout = None):
+        pass
 
     def _init_state(self):
-        
-        def init_state():
-            salt_opts = {
-                'file_client':       'local',
-                'renderer':          'yaml_jinja',
-                'failhard':          False,
-                'state_top':         'salt://top.sls',
-                'nodegroups':        {},
-                'file_roots':        {'base': ['/srv/salt']},
-                'state_auto_order':  False,
-                'extension_modules': '/var/cache/salt/minion/extmods',
-                'id':                '',
-                'pillar_roots':      '',
-                'cachedir':          '/code/OpsAgent/cache',
-                'test':              False,
-                }
-            
-            self.state = State(salt_opts)
-
-            print json.dumps(salt_opts, sort_keys=True,
-                  indent=4, separators=(',', ': '))
-
-        self._pool.spawn(init_state)
-        gevent.wait()
+        ret = self._pool.spawn(lambda opts: State(opts), self._opts)
+        ret.wait()
+        self.state = ret.get()
 
 # codes for test
 def main():
+
+    import json
+
+    salt_opts = {
+        'file_client':       'local',
+        'renderer':          'yaml_jinja',
+        'failhard':          False,
+        'state_top':         'salt://top.sls',
+        'nodegroups':        {},
+        'file_roots':        {'base': ['/srv/salt']},
+        'state_auto_order':  False,
+        'extension_modules': '/var/cache/salt/minion/extmods',
+        'id':                '',
+        'pillar_roots':      '',
+        'cachedir':          '/code/OpsAgent/cache',
+        'test':              False,
+        }
+
     predefine_states = {
         'mypackages' : {
             '__env__': 'base',
@@ -103,8 +106,13 @@ def main():
         }
     }
 
-    runner = StateRunner([predefine_states, clean_up_states])
-    runner.run()
+    runner = StateRunner(salt_opts, [predefine_states, clean_up_states])
+    print json.dumps(runner.get_opts(), sort_keys=True,
+          indent=4, separators=(',', ': '))
+
+    ret = runner.run()
+    print json.dumps(ret, sort_keys=True,
+          indent=4, separators=(',', ': '))
 
 if __name__ == '__main__':
     main()
