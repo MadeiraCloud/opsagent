0e22f5c6120aead625258482b5eb3977b39c28fb
Thibault BRONCHAIN <thibault@mc2.io>
2014-01-25 11:29:00 +0800

    remove states

--- salt/modules/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-Execution Module Directory
-'''
-
-def state_std(kwargs, res):
-	if kwargs and kwargs.has_key('state_ret'):
-		kwargs['state_ret']['state_stdout'] += res['stdout'] + '\n'
