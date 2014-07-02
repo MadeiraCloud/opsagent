55f9bfdaa3f4d3c5dfa126212088a4cc85421101
Thibault BRONCHAIN <thibault@mc2.io>
2014-07-02 11:55:53 +0800

    improve log

--- sources/opsagent/state/worker.py
+++ sources/opsagent/state/worker.py
@@ -108,7 +108,7 @@ class StateWorker(threading.Thread):
             else:
                 if sent:
                     success = True
-                    utils.log("DEBUG", "Data successfully sent.",('__send',self))
+                    utils.log("DEBUG", "Data successfully sent",('__send',self))
                 else:
                     utils.log("WARNING", "Data not sent, retrying in %s seconds..."%(WAIT_RESEND),('__send',self))
                     time.sleep(WAIT_RESEND)
@@ -160,10 +160,10 @@ class StateWorker(threading.Thread):
         if self.__cv_wait:
             utils.log("DEBUG", "Aquire conditional lock ...",('abort',self))
             self.__cv.acquire()
-            utils.log("DEBUG", "Conditional lock acquired.",('abort',self))
-            utils.log("DEBUG", "Notify execution thread.",('abort',self))
+            utils.log("DEBUG", "Conditional lock acquired",('abort',self))
+            utils.log("DEBUG", "Notify execution thread",('abort',self))
             self.__cv.notify()
-            utils.log("DEBUG", "Release conditional lock.",('abort',self))
+            utils.log("DEBUG", "Release conditional lock",('abort',self))
             self.__cv.release()
 
 
@@ -189,7 +189,7 @@ class StateWorker(threading.Thread):
                     e = str(e)
                     if e.find("No such process"):
                         self.__delaypid = None
-                        utils.log("DEBUG", "Delay process killed.",('__kill_delay',self))
+                        utils.log("DEBUG", "Delay process killed",('__kill_delay',self))
                         break
                     else:
                         utils.log("WARNING", "Error killing delay: %s"%(e),('__kill_delay',self))
@@ -198,7 +198,7 @@ class StateWorker(threading.Thread):
                     self.__delaypid = None
                     break
         else:
-            utils.log("DEBUG", "Recipe not in delay process.",('__kill_delay',self))
+            utils.log("DEBUG", "Recipe not in delay process",('__kill_delay',self))
 
     # Kill child process (obsolete?)
     def __kill_childs(self):
@@ -218,12 +218,12 @@ class StateWorker(threading.Thread):
                     if re.search(r'PPid.*%s'%(cur_pid), line):
                         utils.log("INFO", "State execution process found #%s. Killing ..."%(pid),('__kill_childs',self))
                         os.killpg(int(pid),signal.SIGKILL)
-                        utils.log("DEBUG", "Process killed.",('kill',self))
+                        utils.log("DEBUG", "Process killed",('kill',self))
                         flag = True
             except Exception as e:
-                utils.log("DEBUG", "Kill child error on pid #%s: '%s'."%(pid,e),('__kill_childs',self))
+                utils.log("DEBUG", "Kill child error on pid #%s: '%s'"%(pid,e),('__kill_childs',self))
         if not flag:
-            utils.log("INFO", "No state execution found.",('__kill_childs',self))
+            utils.log("INFO", "No state execution found",('__kill_childs',self))
         return True
 
     # Kill execution
@@ -249,7 +249,7 @@ class StateWorker(threading.Thread):
                     self.__executing.terminate()
                 except Exception: pass
         else:
-            utils.log("DEBUG", "Execution not running.",('__kill_exec',self))
+            utils.log("DEBUG", "Execution not running",('__kill_exec',self))
 
     # Halt wait
     def __kill_wait(self):
@@ -262,15 +262,15 @@ class StateWorker(threading.Thread):
     # Kill the current execution
     def kill(self):
         if not self.__run:
-            utils.log("DEBUG", "Execution not running, nothing to do.",('kill',self))
+            utils.log("DEBUG", "Execution not running, nothing to do",('kill',self))
             return
         while self.__run or self.__executing:
-            utils.log("DEBUG", "Sending stop execution signal.",('kill',self))
+            utils.log("DEBUG", "Sending stop execution signal",('kill',self))
             self.__run = False
             self.__kill_delay()
             self.__kill_wait()
             self.__kill_exec()
-            utils.log("INFO", "Execution killed.",('kill',self))
+            utils.log("INFO", "Execution killed",('kill',self))
     ##
 
 
@@ -305,32 +305,32 @@ class StateWorker(threading.Thread):
         reload(opsagent.state.runner)
         from opsagent.state.runner import StateRunner
         self.__state_runner = StateRunner(config=self.__config['salt'])
-        utils.log("DEBUG", "Modules loaded.",('load_modules',self))
+        utils.log("DEBUG", "Modules loaded",('load_modules',self))
 
     # Load new recipe
     def load(self, version=None, states=None):
         utils.log("DEBUG", "Aquire conditional lock ...",('load',self))
         self.__cv.acquire()
-        utils.log("DEBUG", "Conditional lock acquired.",('load',self))
+        utils.log("DEBUG", "Conditional lock acquired",('load',self))
         self.__version = version
 
         self.__recipe_count = (self.__recipe_count+1 if self.__recipe_count < RECIPE_COUNT_RESET else 0)
         exp = None
         try:
             if states:
-                utils.log("INFO", "Loading new states.",('load',self))
+                utils.log("INFO", "Loading new states",('load',self))
                 del self.__states
                 self.__states = copy.deepcopy(states)
             else:
-                utils.log("INFO", "No change in states.",('load',self))
-            utils.log("DEBUG", "Allow to run.",('load',self))
+                utils.log("INFO", "No change in states",('load',self))
+            utils.log("DEBUG", "Allow to run",('load',self))
             self.__run = True
         except Exception as e:
             exp = e
 
-        utils.log("DEBUG", "Notify execution thread.",('load',self))
+        utils.log("DEBUG", "Notify execution thread",('load',self))
         self.__cv.notify()
-        utils.log("DEBUG", "Release conditional lock.",('load',self))
+        utils.log("DEBUG", "Release conditional lock",('load',self))
         self.__cv.release()
 
         if exp: raise OpsAgentException(exp)
@@ -340,7 +340,7 @@ class StateWorker(threading.Thread):
     ## WAIT PROCESS
     # Add state to done list
     def state_done(self, sid):
-        utils.log("DEBUG", "Adding id '%s' to done states list."%(sid),('state_done',self))
+        utils.log("DEBUG", "Adding id '%s' to done states list"%(sid),('state_done',self))
         self.__done.append(sid)
         self.__wait_event.set()
     ##
@@ -358,7 +358,7 @@ class StateWorker(threading.Thread):
             utils.log("INFO", "New state status received, analysing ...",('__exec_wait',self))
         if sid in self.__done:
             value = SUCCESS
-            utils.log("INFO", "Waited state completed.",('__exec_wait',self))
+            utils.log("INFO", "Waited state completed",('__exec_wait',self))
         else:
             value = FAIL
             utils.log("WARNING", "Waited state ABORTED.",('__exec_wait',self))
@@ -372,17 +372,17 @@ class StateWorker(threading.Thread):
         if parameter and type(parameter) is dict and parameter.get("watch"):
             watchs = parameter.get("watch")
             if type(watchs) is list:
-                utils.log("DEBUG", "Watched state detected.",('__exec_salt',self))
+                utils.log("DEBUG", "Watched state detected",('__exec_salt',self))
                 del parameter["watch"]
                 for watch in watchs:
                     try:
-                        utils.log("DEBUG", "Watched file '%s' found."%(watch),('__exec_salt',self))
+                        utils.log("DEBUG", "Watched file '%s' found"%(watch),('__exec_salt',self))
                         cs = Checksum(watch,sid,self.__config['global']['watch'])
                         if cs.update():
                             parameter["watch"] = True
                             utils.log("INFO","Watch event triggered, replacing standard action ...",('__exec_salt',self))
                     except Exception as e:
-                        err = "Internal error while watch process on file '%s': %s."%(watch,e)
+                        err = "Internal error while watch process on file '%s': %s"%(watch,e)
                         utils.log("ERROR", err,('__exec_salt',self))
                         res['result'] = FAIL
                         res['comment'] = err
@@ -404,7 +404,7 @@ class StateWorker(threading.Thread):
             res['out_log'] = None
             return
 
-        utils.log("INFO", "State ID '%s' from module '%s' done, result '%s'."%(sid,module,result),('__exec_salt',self))
+        utils.log("INFO", "State ID '%s' from module '%s' done, result '%s'"%(sid,module,result),('__exec_salt',self))
         utils.log("DEBUG", "State out log='%s'"%(out_log),('__exec_salt',self))
         utils.log("DEBUG", "State comment='%s'"%(comment),('__exec_salt',self))
         res['result'] = result
@@ -413,7 +413,7 @@ class StateWorker(threading.Thread):
 
     # Delay at the end of the states
     def __recipe_delay(self):
-        utils.log("INFO", "Last state reached, execution paused for %s minutes."%(self.__config['salt']['delay']),('__recipe_delay',self))
+        utils.log("INFO", "Last state reached, execution paused for %s minutes"%(self.__config['salt']['delay']),('__recipe_delay',self))
         self.__delaypid = os.fork()
         if (self.__delaypid == 0): # son
             time.sleep(int(self.__config['salt']['delay'])*60)
@@ -466,20 +466,20 @@ class StateWorker(threading.Thread):
                 del mem_manager
                 del results
 
-                utils.log("DEBUG", "State runner process terminated.",('__run_state',self))
+                utils.log("DEBUG", "State runner process terminated",('__run_state',self))
         except SWWaitFormatException:
             utils.log("ERROR", "Wrong wait request",('__run_state',self))
             (result,comment,out_log) = (FAIL,"Wrong wait request",None)
         except Exception as e:
-            utils.log("ERROR", "Unknown exception: '%s'."%(e),('__run_state',self))
-            (result,comment,out_log) = (FAIL,"Internal error: '%s'."%(e),None)
+            utils.log("ERROR", "Unknown exception: '%s'"%(e),('__run_state',self))
+            (result,comment,out_log) = (FAIL,"Internal error: '%s'"%(e),None)
         return (result,comment,out_log)
 
     # Runner init phase
     def __runner_init(self):
         # check empty list
         if not self.__states:
-            utils.log("WARNING", "Empty states list.",('__runner',self))
+            utils.log("WARNING", "Empty states list",('__runner',self))
             self.__run = False
             return False
 
@@ -489,8 +489,8 @@ class StateWorker(threading.Thread):
                 # Load modules on each round
                 self.__load_modules()
             except Exception:
-                utils.log("WARNING", "Can't load states modules.",('__runner',self))
-                err="Can't load states modules."
+                utils.log("WARNING", "Can't load states modules",('__runner',self))
+                err="Can't load states modules"
         if not self.__config['runtime']['clone']:
             err = "Can't clone states repo"
         elif not self.__config['runtime']['tag']:
@@ -521,7 +521,7 @@ class StateWorker(threading.Thread):
 
             # Transmit results
             if self.__run:
-                utils.log("INFO", "Execution complete, reporting logs to backend.",('__runner',self))
+                utils.log("INFO", "Execution complete, reporting logs to backend",('__runner',self))
                 # send result to backend
                 sent = self.__send(send.statelog(init=self.__config['init'],
                                                  version=self.__version,
@@ -534,7 +534,7 @@ class StateWorker(threading.Thread):
                     # global status iteration
                     self.__status += 1
                     if self.__status >= len(self.__states):
-                        utils.log("INFO", "All good, last state succeed! Back to first one.",('__runner',self))
+                        utils.log("INFO", "All good, last state succeed! Back to first one",('__runner',self))
                         # not terminating, wait before next round
                         if not self.__abort:
                             self.__recipe_delay()
@@ -544,7 +544,7 @@ class StateWorker(threading.Thread):
                             self.__run = False
                     else:
                         time.sleep(WAIT_STATE)
-                        utils.log("INFO", "All good, switching to next state.",('__runner',self))
+                        utils.log("INFO", "All good, switching to next state",('__runner',self))
                 # state failed
                 else:
                     self.__status = 0
@@ -554,7 +554,7 @@ class StateWorker(threading.Thread):
                         utils.log("WARNING", "Something went wrong, retrying current state in %s seconds"%(WAIT_STATE_RETRY),('__runner',self))
                         time.sleep(WAIT_STATE_RETRY)
             else:
-                utils.log("WARNING", "Execution aborted.",('__runner',self))
+                utils.log("WARNING", "Execution aborted",('__runner',self))
 
 
     # Callback on start
@@ -570,14 +570,14 @@ class StateWorker(threading.Thread):
                 utils.log("DEBUG", "Ready to go ...",('run',self))
                 self.__runner()
             except Exception as e:
-                utils.log("ERROR", "Unexpected error: %s."%(e),('run',self))
+                utils.log("ERROR", "Unexpected error: %s"%(e),('run',self))
             self.__reset()
             self.__cv.release()
         utils.log("WARNING", "Exiting...",('run',self))
         if self.__manager:
             utils.log("INFO", "Stopping manager...",('run',self))
             self.__manager.stop()
-            utils.log("INFO", "Manager stopped.",('run',self))
-        utils.log("WARNING", "Terminated.",('run',self))
+            utils.log("INFO", "Manager stopped",('run',self))
+        utils.log("WARNING", "Terminated",('run',self))
     ##
 ##
