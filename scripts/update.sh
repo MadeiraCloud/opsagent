0fc17cbde2558f718538d00fd5f00d181a14b099
Thibault BRONCHAIN <thibault@mc2.io>
2014-07-06 15:14:36 +0800

    merge master

--- scripts/update.sh
+++ scripts/update.sh
@@ -29,36 +29,38 @@ export APP_ID=$2
 export VERSION=$3
 export BASE_REMOTE=$4
 export GPG_KEY_URI=$5
+export AGENT_UPDATE="update"
 
 OA_REMOTE="${BASE_REMOTE}/${VERSION}"
 
-wget -nv -O ${OA_CONF_DIR}/userdata.sh.gpg ${OA_REMOTE}/userdata.sh.gpg
-wget -nv -O ${OA_CONF_DIR}/userdata.sh.gpg.cksum ${OA_REMOTE}/userdata.sh.gpg.cksum
+wget -nv -O ${OA_CONF_DIR}/ud_init.sh.gpg ${OA_REMOTE}/ud_init.sh.gpg
+wget -nv -O ${OA_CONF_DIR}/ud_init.sh.gpg.cksum ${OA_REMOTE}/ud_init.sh.gpg.cksum
 
 cd ${OA_CONF_DIR}
-REF_CKSUM="$(cat ${OA_CONF_DIR}/userdata.sh.gpg.cksum)"
-CUR_CKSUM="$(cksum userdata.sh.gpg)"
+REF_CKSUM="$(cat ${OA_CONF_DIR}/ud_init.sh.gpg.cksum)"
+CUR_CKSUM="$(cksum ud_init.sh.gpg)"
 cd -
 if [ "$REF_CKSUM" = "$CUR_CKSUM" ]; then
-    chmod 640 ${OA_CONF_DIR}/userdata.sh.gpg
+    chmod 640 ${OA_CONF_DIR}/ud_init.sh.gpg
 
     gpg --no-tty --import ${OA_GPG_KEY}
-    gpg --no-tty --verify ${OA_CONF_DIR}/userdata.sh.gpg
+    gpg --no-tty --verify ${OA_CONF_DIR}/ud_init.sh.gpg
     if [ $? -eq 0 ]; then
-        gpg --no-tty --output ${OA_CONF_DIR}/userdata.sh --decrypt ${OA_CONF_DIR}/userdata.sh.gpg
-        chmod 750 ${OA_CONF_DIR}/userdata.sh
-        bash ${OA_CONF_DIR}/userdata.sh "update"
+        gpg --no-tty --output ${OA_CONF_DIR}/ud_init.sh --decrypt ${OA_CONF_DIR}/ud_init.sh.gpg
+        chmod 750 ${OA_CONF_DIR}/ud_init.sh
+        bash ${OA_CONF_DIR}/ud_init.sh
         EXIT=$?
     else
-        echo "update.sh: FATAL: userdata GPG extraction failed." >&2
+        echo "update.sh: FATAL: ud_init GPG extraction failed." >&2
         EXIT=10
    fi
 else
-    echo "update.sh: FATAL: can't verify userdata script." >&2
+    echo "update.sh: FATAL: can't verify ud_init script." >&2
     EXIT=11
 fi
 
 rm -f ${OA_UPDATE_FILE}
+unset ${AGENT_UPDATE}
 
 if [ $EXIT -eq 0 ]; then
     echo "Update succeed."
