b503cf48c85cdb028d2315e0a837d41f2260ff07
Thibault BRONCHAIN <thibault@mc2.io>
2014-07-10 18:59:36 +0800

    rollback

--- scripts/ud_init.sh
+++ scripts/ud_init.sh
@@ -20,8 +20,6 @@ OA_ENV_DIR=${OA_ROOT_DIR}/env
 OA_EXEC_FILE=/tmp/opsagent.boot
 OA_AGENT=opsagent
 
-crontab -r
-
 if [ "${AGENT_UPDATE}" != "update" ]; then
     # disable agent during bootstrap, if not update
     if [ $(which chkconfig 2> /dev/null) ]; then
