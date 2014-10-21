140adaae610583930959188d22ee2d6119ac9020
Thibault BRONCHAIN <thibault@mc2.io>
2014-10-21 22:03:05 +0800

    fix update

--- scripts/init.sh
+++ scripts/init.sh
@@ -259,8 +259,11 @@ fi
 
 # load agent
 if [ ${UPDATE_AGENT} -ne 0 ]; then
-    echo "load agent after update"
-    service opsagentd start
+    service opsagentd status
+    if [ $? -eq 0 ]; then
+        echo "kill agent after update"
+        service opsagentd force-stop
+    fi
 fi
 
 # run check
