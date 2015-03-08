7989123754a2e25fcde45266abbd965b5870696c
Thibault BRONCHAIN <thibault@bronchain.me>
2015-03-08 17:15:59 +0800

    improve states

--- tests/states.sh
+++ tests/states.sh
@@ -2,9 +2,15 @@
 # States Unit Tests bridge
 
 TEST_DIR="/opt/visualops"
+service opsagentd status
+STATUS=$?
+service opsagentd stop
 cd ${TEST_DIR}/bootstrap/salt
 git checkout develop
 git stash
 git pull
-cd test
+cd ${TEST_DIR}/bootstrap/salt/test
 ./run.sh 3
+if [ $STATUS -eq 0 ]; then
+    service opsagentd start
+fi
